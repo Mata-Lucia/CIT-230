f6e01968d5eb730875272a8295ed37a148400df0
luciamata5 <mat18013@byui.edu>
2019-03-17 23:35:31 -0600

    update weather summary

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,10 +1,11 @@
+function windchill() {
 
-
-let temp = parseInt(document.getElementById("one").innerHTML);
-let speed = parseInt(document.getElementById("two").innerHTML);
+let temp = parseInt(document.getElementById("high").innerHTML);
+let speed = parseInt(document.getElementById("wind").innerHTML);
 
 result = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16)
          + 0.4275 * temp * Math.pow(speed, 0.16);
 
 document.getElementById('output').innerHTML = result.toFixed(1);
 
+}
